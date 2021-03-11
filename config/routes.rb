d81b48b63e3ab61140806c556bc2e4997dd189d4
keisuketakahashi514 <takahashikeisuke.0514@gmail.com>
2021-03-11 01:41:45 +0000

    [Finish] ファイルの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
-  resources :books, only: [:create, :index, :show, :destroy]
-  resources :users, only: [:show]
+  get 'about' => 'homes#about' , as: 'about'
+  resources :books, only: [:create, :index, :show, :edit, :destroy]
+  resources :users, only: [:show, :index, :edit, :create]
 
 
 
